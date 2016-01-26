c3e2459b82fab5059a3c84648569d4e0717c99a2
kcent <kristi@theresnobox.net>
2016-01-26 16:08:17 -0500

    やった！

--- js/templates.js
+++ js/templates.js
@@ -1,5 +1,8 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['blank.hbs'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "";
+},"useData":true});
 templates['error.hbs'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper;
 
@@ -74,8 +77,8 @@ templates['worksheet/base.hbs'] = template({"compiler":[7,">= 4.0.0"],"main":fun
 templates['worksheet/template.hbs'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper;
 
-  return "<div class=\"target\">"
+  return "<div class=\"col-md-12 formula-list-item\">\n	<div class=\"target\">"
     + ((stack1 = ((helper = (helper = helpers.hex || (depth0 != null ? depth0.hex : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"hex","hash":{},"data":data}) : helper))) != null ? stack1 : "")
-    + "</div>\n<svg class=\"stroke-order\"></svg>";
+    + "</div>\n	<div class=\"stroke-order\"></div>\n</div>";
 },"useData":true});
 })();
